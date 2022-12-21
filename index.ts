3d1a1806cb22889e572e23b49f11e7f0edb0e8ee
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-21 15:42:42 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { interval, Subject } from 'rxjs';
 import { share, take, tap } from 'rxjs/operators';
 
 const source = interval(1000).pipe(
-  take(3),
+  take(2),
   tap((x) => console.log('Emits: ', x)),
 
   //Comment share to see the difference in the output
@@ -10,6 +10,9 @@ const source = interval(1000).pipe(
 );
 
 //Note : Mark the comma between subscriptions
-setTimeout(() =>
-source.subscribe((v) => console.log('subscription 1: ', v)),1500),
+setTimeout(
+  () => source.subscribe((v) => console.log('subscription 1: ', v)),
+  1500
+),
   source.subscribe((v) => console.log('subscription 2: ', v));
+//source.pipe(take(1)).subscribe((v) => console.log('subscription 3: ', v));
