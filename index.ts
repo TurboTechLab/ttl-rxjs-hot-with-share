18f2c5628899bce9e5c51f1f0d6b554e79bd8dca
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2023-01-18 09:10:28 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -12,8 +12,9 @@ const source = interval(1000).pipe(
 
 //Note : Mark the comma between subscriptions
 source.subscribe((v) => console.log('subscription1: ', v)),
+//source.pipe(take(1)).subscribe((v) => console.log('subscription1: ', v)),
 setTimeout(
   () => source.subscribe((v) => console.log('subscription2: ', v)),
   1500
 );
- //source.pipe(take(1)).subscribe((v) => console.log('subscription3: ', v));
+ 
